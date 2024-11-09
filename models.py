9ab28f9a73dc4a7c5d8e9dde12ed9d8ff0a90c2c
Kvadrokom <lortrem@mail.ru>
2024-11-09 23:47:41 +0300

    stepic_v1: async table todos first commit

--- models.py
+++ models.py
@@ -1,12 +1,24 @@
 from pydantic import BaseModel, PositiveInt
 from typing import Union
 
-class User(BaseModel):
-    username: str
-    password: str
-
+class TodoReturn(BaseModel):
+    title: str
+    description: str
+    complited: bool = False
 
 class Todo(BaseModel):
+    id: int
+    title: str
+    description: str
+    complited: bool = False
+
+
+class TodoCreate(BaseModel):
+    title: str
+    description: str
+
+
+class TodoUpdate(BaseModel):
     title: str | None = None
     description: str | None = None
     complited: bool = False
